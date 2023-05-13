6969908e849d75c85a3881a6c0561ef8f8cb6fc4
astasovsky <stasovsky.ua@gmail.com>
2023-05-13 14:30:55 +0300

    Example 2. Done

---
+++ Example2/Program.cs
@@ -0,0 +1,29 @@
+﻿int x = 0;
+object locker = new(); // об'єкт-заглушка
+// запускаємо п'ять потоків
+for (int i = 1; i < 6; i++)
+{
+    Thread myThread = new(Print);
+    myThread.Name = $"Потік {i}";
+    myThread.Start();
+}
+
+void Print()
+{
+    bool acquiredLock = false;
+    try
+    {
+        Monitor.Enter(locker, ref acquiredLock);
+        x = 1;
+        for (int i = 1; i < 6; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+            x++;
+            Thread.Sleep(100);
+        }
+    }
+    finally
+    {
+        if (acquiredLock) Monitor.Exit(locker);
+    }
+}
